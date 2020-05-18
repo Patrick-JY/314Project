8405182c0827472a19110988ebfb4b90029ed002
Kan <kan@DESKTOP-V9DN3VH.localdomain>
2020-05-18 23:07:37 +0700

    Import change

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,24 +1,27 @@
 import argparse
+import importlib
 
 import pytest
 
-from src.pulling_cli import *
+import src.pulling_cli as cli
 
 
 class Test:
     def setup_class(self):
-        self.parser = create_parser()
+        self.parser = cli.create_parser()
 
     def test_argsi(self):
         print('test argument -i')
-        parsed = self.parser.parse_args(['-i','10'])
+        parsed = self.parser.parse_args(['-i', '10'])
         assert parsed.amount == 10
+
     def test_argsinput(self):
         print('test argument --input')
-        parsed = self.parser.parse_args(['--input','50'])
+        parsed = self.parser.parse_args(['--input', '50'])
         assert parsed.amount == 50
-    def test_mock_parser(self):
-        self.parser.add_argument('-o','--output',action="store_true",dest="output",default=False)
-        parsed = self.parser.parse_args(['-i','10','-o'])
-        assert parsed.output == True
 
+    def test_mock_parser(self):
+        self.parser.add_argument('-o', '--output',
+                                 action="store_true", dest="output", default=False)
+        parsed = self.parser.parse_args(['-i', '10', '-o'])
+        assert parsed.output == True
