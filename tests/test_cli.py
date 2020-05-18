d7d5e77bba1e327033f25886317b8477046ed567
Kan <kan@DESKTOP-V9DN3VH.localdomain>
2020-05-18 22:45:39 +0700

    Convert to pytest

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,21 +1,24 @@
-import unittest as test
-from src.pulling_cli import *
 import argparse
-class ParserTest(test.TestCase):
-    def setUp(self):
+
+import pytest
+
+from src.pulling_cli import *
+
+
+class Test:
+    def setup_class(self):
         self.parser = create_parser()
 
     def test_argsi(self):
         print('test argument -i')
         parsed = self.parser.parse_args(['-i','10'])
-        self.assertEqual(parsed.amount,10)
+        assert parsed.amount == 10
     def test_argsinput(self):
         print('test argument --input')
         parsed = self.parser.parse_args(['--input','50'])
-        self.assertEqual(parsed.amount, 50)
+        assert parsed.amount == 50
     def test_mock_parser(self):
         self.parser.add_argument('-o','--output',action="store_true",dest="output",default=False)
         parsed = self.parser.parse_args(['-i','10','-o'])
-        self.assertTrue(parsed.output)
-
+        assert parsed.output == True
 
