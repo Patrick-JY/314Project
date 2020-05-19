52afefd62636ff6c84f5a34eea18f058121837ec
Matthew Ruse <matthew.ruse@gmail.com>
2020-05-19 14:11:58 +1000

    Iter 1: Added test for capitalize_all_words

---
+++ tests/test_testing.py
@@ -0,0 +1,14 @@
+import pytest
+import json
+import pandas as pd
+from src.testing_logic import capitalize_all_words
+
+@pytest.fixture
+def df():
+    # replace with pulling amazon data
+    dataframe = pd.read_json("../json/githubdata.json", orient="records", lines=True)
+    return dataframe[["reviewerID","reviewText", "overall"]]
+
+def test_capitalize_all_words(df):
+    capitalize_all_words(df)
+    assert df["reviewTextUpper"].filter(lambda x: not x.isUpper()) == 0
