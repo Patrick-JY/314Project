4c50ee113a25ede44d205d27d075f04ff06c557b
Matthew Ruse <matthew.ruse@gmail.com>
2020-05-22 13:55:08 +1000

    Iter 2: Added test for uncapitalize_all_words and added logic for uncapitalize_all_words

--- src/testing_logic.py
+++ src/testing_logic.py
@@ -1,2 +1,6 @@
 def capitalise_all_words(df):
     df['reviewTextUpper'] = df['reviewText'].str.upper()
+
+def uncapitalise_all_words(df):
+    df['reviewTextUpper'] = df['reviewText'].str.lower()
+
--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,7 +1,7 @@
 import pytest
 import json
 import pandas as pd
-from src.testing_logic import capitalise_all_words
+from src.testing_logic import capitalise_all_words, uncapitalise_all_words
 
 @pytest.fixture
 def df():
@@ -13,3 +13,8 @@ def test_capitalise_all_words(df):
     capitalise_all_words(df)
     capitalised_words = df[~df.reviewTextUpper.str.isupper()]
     assert capitalised_words.empty, "Word capitalisation failed, {0} word(s) were not capitalised ".format(len(capitalised_words))
+
+def test_uncapitalise_all_words(df):
+    uncapitalise_all_words(df)
+    uncapitalise_words = df[~df.reviewTextUpper.str.islower()]
+    assert uncapitalise_words.empty, "Word uncapitalisation failed, {0} word(s) were capitalised ".format(len(uncapitalised_words))
