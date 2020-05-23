3e310cb2f30d30e61f3c5cce3c662e40dc098956
Matthew Ruse <matthew.ruse@gmail.com>
2020-05-23 13:26:14 +1000

    Iter 3: Updated testing branch to use pulling_amazon in test

--- src/testing_logic.py
+++ src/testing_logic.py
@@ -1,6 +1,6 @@
 def capitalise_all_words(df):
-    df['reviewTextUpper'] = df['reviewText'].str.upper()
+    df['ReviewTextUpper'] = df['ReviewText'].str.upper()
 
 def uncapitalise_all_words(df):
-    df['reviewTextUpper'] = df['reviewText'].str.lower()
+    df['ReviewTextUpper'] = df['ReviewText'].str.lower()
 
