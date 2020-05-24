10cc9c61bb31bc0c3b033a653820d6cd46b49c56
Matthew Ruse <matthew.ruse@gmail.com>
2020-05-24 15:42:25 +1000

    Update test to fix error in dictionary iteration Create code for load_vadersentiment_lexicon_data to pass the test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,6 +9,6 @@ def test_load_vadersentiment_lexicon_data():
     lexicon_data = load_vadersentiment_lexicon_data()
     assert lexicon_data is not None, "No lexicon data was loaded"
     assert type(lexicon_data) == dict, "Lexicon data should be a data dictionary"
-    for key, value in lexicon_data.keys():
+    for key, value in lexicon_data.items():
         assert type(key) == str, "key should be a string"
         assert type(value) == float, "value should be a float"
