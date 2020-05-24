9a5fb729b752872bfa912f36624eb05f150f3e5b
Matthew Ruse <matthew.ruse@gmail.com>
2020-05-24 15:34:30 +1000

    Create test for load load_vadersentiment_lexicon_data

--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,6 @@ from pathlib import Path
 
 def join_base_path(path):
     return Path(__file__).parent.parent.joinpath(path)
+
+def load_vadersentiment_lexicon_data():
+    pass
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,14 @@
-from src.utils import join_base_path
+from src.utils import join_base_path, load_vadersentiment_lexicon_data
 import os
 
 def test_join_base_path():
     assert os.path.exists(join_base_path(".314ProjectBaseDir")) == True
     assert os.path.exists(join_base_path(".314ProjectBaseDirs")) == False
+
+def test_load_vadersentiment_lexicon_data():
+    lexicon_data = load_vadersentiment_lexicon_data()
+    assert lexicon_data is not None, "No lexicon data was loaded"
+    assert type(lexicon_data) == dict, "Lexicon data should be a data dictionary"
+    for key, value in lexicon_data.keys():
+        assert type(key) == str, "key should be a string"
+        assert type(value) == float, "value should be a float"
