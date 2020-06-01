82f5e3321d42487087fb42e4750bca68d5bcfc01
Patrick <patrickjyoung97@gmail.com>
2020-06-01 16:19:38 +1000

    Random Iter- Just changing the synonym functionality so it works better

--- src/testing_logic.py
+++ src/testing_logic.py
@@ -2,9 +2,11 @@ from src.sentiment_analyser_interface import performSentimentAnalysis
 from nltk import sent_tokenize
 from nltk.corpus import wordnet as wn
 from nltk import sent_tokenize
+from nltk.tokenize import TweetTokenizer
 import nltk
 nltk.download("punkt", quiet=True)
 nltk.download("wordnet", quiet=True)
+nltk.download('averaged_perceptron_tagger', quiet=True)
 import random
 
 from src.utils import get_positive_words, get_negative_words
@@ -83,40 +85,65 @@ def replace_with_synonyms(data_frame):
     word_list = positive_words + negative_words
     data_frame['SynonymReplaced'] = data_frame['ReviewText'].apply(lambda row: synonym_replacer(row, word_list))
 
+
+# Code for this function thanks to https://stackoverflow.com/questions/15586721/wordnet-lemmatization-and-pos-tagging-in-python
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wn.ADJ
+    elif treebank_tag.startswith('V'):
+        return wn.VERB
+    elif treebank_tag.startswith('N'):
+        return wn.NOUN
+    elif treebank_tag.startswith('R'):
+        return wn.ADV
+    else:
+        return wn.NOUN  # Noun is probably the safest default tbh
+
+
+
 def synonym_replacer(text, word_list):
     sentences = sent_tokenize(text)
+    tokenizer = TweetTokenizer()
     result = ""
     for sentence in sentences:
         result_words = []
-        words = sentence.split(" ")
-        for word in words:
-            word_stripped_lower = word.replace(".", "").replace(",", "").replace(";", "").lower()
-            if word in word_list:
+        pos_tag = nltk.pos_tag(tokenizer.tokenize(sentence))
+
+        words = pos_tag
+
+        for tuple_word in words:
+            word = list(tuple_word)
+
+            word_stripped_lower = word  # Setting it equal so all the second elements are the same
+
+            word_stripped_lower[0] = word[0].lower()
+
+            if word[0] in word_list:
                 synonyms = []
-                for syn in wn.synsets(word_stripped_lower):
+                for syn in wn.synsets(word_stripped_lower[0], get_wordnet_pos(word_stripped_lower[1])):
                     for l in syn.lemmas():
+
                         # Skip words that are not in word_list as VaderSentiment can't evaluate these
                         # Skip words with _ as these can't be parsed either by VaderSentiment
                         # Skip words that are equal to the original word or are plurals etc. (Need to work on this to exclude all)
-                        # https://stackoverflow.com/questions/14489309/convert-words-between-verb-noun-adjective-forms
-                        # https://stackoverflow.com/questions/32411594/identify-the-word-as-a-noun-verb-or-adjective
-                        if l.name() not in word_list or l.name().lower() in word_stripped_lower or "_" in l.name():
+                        if l.name() not in word_list or l.name().lower() in word_stripped_lower[0] or "_" in l.name():
                             continue
                         synonyms.append((l.name()))
                 if synonyms:
                     # fix adding punctuation at end of word
                     add_end_letter = ""
-                    if word.endswith(","):
+                    if word[0].endswith(","):
                         add_end_letter = ","
-                    if word.endswith(";"):
+                    if word[0].endswith(";"):
                         add_end_letter = ";"
-                    word = random.choice(synonyms) + add_end_letter
-            result_words.append(word)
-        if result != "":
+                    word[0] = random.choice(synonyms) + add_end_letter
+            result_words.append(word[0])
+        if result != ("", ".", ",", ";", "\""):
             result += " "
         result += ' '.join(result_words)
         if sentence.endswith(".") and not result.endswith("."):
             result += "."
+
     return result
 
 def run_sentiment_mr4(df):
--- tests/test_report_creation.py
+++ tests/test_report_creation.py
@@ -2,7 +2,7 @@ import pytest
 import pandas as pd
 
 
-def test_report_generation(df):
+def test_report_generation():
     df = pd.DataFrame
     assert df is not df.empty
     print(df.head(5))
