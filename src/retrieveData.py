d5de390e8b20be675112421a260d810604425608
fitzy1922 <65000394+fitzy1922@users.noreply.github.com>
2020-06-04 14:22:07 +1000

    Merge pull request #23 from Jacob-FF/retrieveData
    Changed so it calculates accuracy against actual reviews. Test file added.

--- src/retrieveData.py
+++ src/retrieveData.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import testing_logic, pulling_logic, report_creation
-
-case_pos = 0
-case_neg = 0
+import testing_logic, pulling_logic
 
 def init():
     # Pulls data from amazon json and prepares for analysis
@@ -14,65 +11,115 @@ def init():
     # Goes through each 'MR' and determines positive and negative outcomes to compare
     df2 = df.loc[:, "Mr0"]
     print('Mr0 (Original):')
-    calculate_data(df2, 0)
+    calculate_data(df,df2, 0)
     df2 = df.loc[:, "Mr1"]
     print('Mr1 (Capitalised):')
-    calculate_data(df2, 1)
+    calculate_data(df,df2, 1)
+    print('Mr1 (Lower Case):')
+    calculate_data(df, df2, 2)
     df2 = df.loc[:, "Mr2"]
-    print('Mr2 (Positive Removed):')
-    calculate_data(df2, 2)
+    print('Mr2 (Positive removed):')
+    calculate_data(df,df2, 3)
     df2 = df.loc[:, "Mr3"]
     print('Mr3 (Negative removed):')
-    calculate_data(df2, 2)
+    calculate_data(df,df2, 3)
 
 
 # Used for calculating overall positive/negative ratio
-def calculate_data(df2,check):
+def calculate_data(df,df2,check):
 
-    pos = 0
-    neg = 0
+    correct = 0
+    pos_correct = 0
+    neg_correct = 0
+    neu_correct = 0
+    k = 0
 
     # Needs a check as one of the dictionaries is categorised in to capitalised and non capitalised
     if check == 0:
+
         for x in df2:
             if x['predicted_sentiment'] == 'positive':
-                pos += 1
-            else:
-                neg += 1
-        overall = pos + neg
-        pos_percent = round(pos / overall * 100, 2)
-        neg_percent = round(neg / overall * 100, 2)
-        print('Positive results: ' + str(pos) + " : " + str(pos_percent) + "%")
-        print('Negative results: ' + str(neg) + " : " + str(neg_percent) + "%")
-        global case_pos
-        global case_neg
-        case_pos = pos_percent
-        case_neg = neg_percent
+                if df['ReviewScore'][k] > 3:
+                    correct += 1
+                    pos_correct += 1
+
+            elif x['predicted_sentiment'] == 'negative':
+                if df['ReviewScore'][k] < 3:
+                    correct += 1
+                    neg_correct += 1
+
+            elif x['predicted_sentiment'] == 'neutral':
+                if df['ReviewScore'][k] == 3:
+                    correct += 1
+                    neu_correct += 1
+
+            k += 1
+
+        f_score = correct / k
+        print("Total reviews: " + str(k) + " | Analysis match: " + str(correct))
+        print("Positive Correct: " + str(pos_correct))
+        print("Negative Correct: " + str(neg_correct))
+        print("Neutral Correct: " + str(neu_correct))
+        print("Accuracy " + str(round(f_score * 100, 2)) + "%")
         return
 
     elif check == 1:
         for x in df2:
             if x['capitalised']['predicted_sentiment'] == 'positive':
-                pos += 1
-            else:
-                neg += 1
+                if df['ReviewScore'][k] > 3:
+                    correct += 1
+
+            elif x['capitalised']['predicted_sentiment'] == 'negative':
+                if df['ReviewScore'][k] < 3:
+                    correct += 1
+
+            elif x['capitalised']['predicted_sentiment'] == 'neutral':
+                if df['ReviewScore'][k] == 3:
+                    correct += 1
+            k += 1
+
+        f_score = correct / k
+        print("Total reviews " + str(k) + " | Analysis match: " + str(correct))
+        print("Accuracy " + str(round(f_score * 100, 2)) + "%")
 
     elif check == 2:
         for x in df2:
+            if x['uncapitalised']['predicted_sentiment'] == 'positive':
+                if df['ReviewScore'][k] > 3:
+                    correct += 1
+
+            elif x['uncapitalised']['predicted_sentiment'] == 'negative':
+                if df['ReviewScore'][k] < 3:
+                    correct += 1
+
+            elif x['uncapitalised']['predicted_sentiment'] == 'neutral':
+                if df['ReviewScore'][k] == 3:
+                    correct += 1
+            k += 1
+
+        f_score = correct / k
+        print("Total reviews " + str(k) + " | Analysis match: " + str(correct))
+        print("Accuracy " + str(round(f_score * 100, 2)) + "%")
+
+    elif check == 3:
+        for x in df2:
             if x['predicted_sentiment'] == 'positive':
-                pos += 1
-            else:
-                neg += 1
+                if df['ReviewScore'][k] > 3:
+                    correct += 1
 
-    overall = pos + neg
-    pos_percent = round(pos/overall * 100, 2)
-    neg_percent = round(neg/overall * 100, 2)
-    margin_of_error = (abs(case_pos - pos_percent) + abs(case_neg - neg_percent)) / 2
+            elif x['predicted_sentiment'] == 'negative':
+                if df['ReviewScore'][k] < 3:
+                    correct += 1
 
-    print('Positive results: ' + str(pos) + " : " + str(pos_percent) + "%")
-    print('Negative results: ' + str(neg) + " : " + str(neg_percent) + "%")
-    print('Difference: ' + str(margin_of_error) + "%")
-    return
+            elif x['predicted_sentiment'] == 'neutral':
+                if df['ReviewScore'][k] == 3:
+                    correct += 1
+
+            k += 1
+
+        f_score = correct / k
+        print("Total reviews: " + str(k) + " | Analysis match: " + str(correct))
+        print("Accuracy " + str(round(f_score * 100, 2)) + "%")
 
 
 init()
---
+++ tests/test_retrieveData.py
@@ -0,0 +1,2 @@
+def test_calculate_data():
+    assert False
