9cc91919079dc8f5b81757232645cab23d5c5eca
Kan <kan@DESKTOP-V9DN3VH.localdomain>
2020-06-05 17:56:08 +0700

    Test 2

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,21 +8,29 @@ class Test:
     def setup_class(self):
         self.parser = cli.create_parser()
 
-    def test_argsi(self):
+    def test_argsinput(self):
         parsed = self.parser.parse_args(['-i', '10'])
         assert parsed.amount == 10
-
-    def test_argsinput(self):
         parsed = self.parser.parse_args(['--input', '50'])
         assert parsed.amount == 50
 
-    def test_inputstring(self):
-        with pytest.raises(SystemExit):
-            self.parser.parse_args(['-i', 'Hi'])
     def test_inputfile(self):
         parsed = self.parser.parse_args(['-i','100','-f','Demo.json'])
         assert parsed.file_input == "Demo.json"
         assert parsed.amount == 100
-    def test_inputfile1(self):
+        parsed = self.parser.parse_args(['--input','10','--file','Amazon.json'])
+        assert parsed.file_input == 'Amazon.json'
         parsed = self.parser.parse_args(['-i','10'])
         assert parsed.file_input == 'Amazon_githubdata.json'
+
+    def test_inputfile3(self):
+        with pytest.raises(SystemExit):
+            self.parser.parse_args(['-i', 'Hi'])
+        with pytest.raises(SystemExit):
+            self.parser.parse_args(['-i','10','--file'])
+        with pytest.raises(SystemExit):
+            self.parser.parse_args(['-f'])
+        with pytest.raises(SystemExit):
+            self.parser.parse_args(['-i'])
+        with pytest.raises(SystemExit):
+            self.parser.parse_args(['--file'])
