53102c7b5f2c039c781741388c36485770d2d71c
Matthew Ruse <matthew.ruse@gmail.com>
2020-06-06 10:07:48 +1000

    Add code for calculate_test3 and check it passes

--- src/analyse_results.py
+++ src/analyse_results.py
@@ -2,7 +2,12 @@ def calculate_test1(df):
     pass
 
 def calculate_test3(df):
-    pass
+    total_rows = len(df)
+    rows_passed = 0
+    for i, row in df.iterrows():
+        if row["Mr2"]["compound"] <= row["Mr0"]["compound"]:
+            rows_passed += 1
+    return 100 * rows_passed / total_rows
 
 def calculate_test4(df):
     total_rows = len(df)
