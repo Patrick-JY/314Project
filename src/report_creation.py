814e7ea254623f109db1ad7db7fed7a4a190b30a
Matthew Ruse <matthew.ruse@gmail.com>
2020-06-08 13:43:58 +1000

    Update test5 accuracy

--- src/report_creation.py
+++ src/report_creation.py
@@ -53,7 +53,7 @@ def report_generation(df):
     test5_result = calculate_test5(df)
     print("Comparing Mr4 within a threshold of the original Dataset")
     print("Mr4 is " + str(test5_result) + "% within a threshold similar to Mr0")
-    if test5_result < 90:
+    if test5_result < 80:
         print("Test 5 failed\n")
     else:
         print("Test 5 passed\n")
