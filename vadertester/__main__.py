7c78a1ad607f7939035df8d3bb75584b5815e2f5
Matthew Ruse <matthew.ruse@gmail.com>
2020-06-08 12:19:46 +1000

    Add print statements to display progress

--- vadertester/__main__.py
+++ vadertester/__main__.py
@@ -7,8 +7,13 @@ def main():
     print("Vader tester started")
     parser = create_parser()
     args = parser.parse_args()
+    print("Loading test data")
     df = random_sample(pulling_amazon(args.file_input), args.amount)
+    print("Finished loading test data")
+    print("Running tests")
     run_tests(df)
+    print("Finished running tests")
+    print("Displaying output")
     report_generation(df)
     print("Vader tester finished")
 
