c7f48d7431afe4fb2a280473d90fd0f3584ba635
Matthew Ruse <matthew.ruse@gmail.com>
2020-06-09 18:04:03 +1000

    Update pulling_cli.py

--- src/pulling_cli.py
+++ src/pulling_cli.py
@@ -5,9 +5,9 @@ from pkg_resources import resource_filename
 def create_parser():
     parser = cliparse.ArgumentParser()
     # number of input data
-    parser.add_argument('-i', '--input', dest='amount', type=int, default=30,
+    parser.add_argument('-i', '--input', dest='amount', type=int, default=5000,
                         required=False, help="Define amount of data to process")
     #file input
-    parser.add_argument('-f', '--file', dest='file_input', type=str, default=resource_filename('vadertester', 'json/Amazon_githubdata.json.gz'),
+    parser.add_argument('-f', '--file', dest='file_input', type=str, default=resource_filename('vadertester', 'json/reviews_Amazon_Instant_Video_5.json.gz'),
                         required=False, help='Input json file to be processed')
     return parser
