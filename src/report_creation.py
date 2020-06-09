0452643c65cdb714674ca45231d1e28e6f397c19
Matthew Ruse <matthew.ruse@gmail.com>
2020-06-09 15:40:50 +1000

    Speed up graph

--- src/report_creation.py
+++ src/report_creation.py
@@ -70,120 +70,37 @@ def report_generation(df):
     plt.show()
 
 def column_graph_word_length(df):
-    graph_df = prepare_word_length(df)
-
-    graph_df.rename(columns={"cat1": "0-50", "cat2": "50-100", "cat3": "100-200", "cat4": "200-300", "cat5": ">300"}, inplace=True)
-
-    ax = graph_df.plot(x="Mr#", y=["0-50", "50-100", "100-200", "200-300", ">300"], kind="bar")
+    df["ReviewTextLength"] = df["ReviewText"].apply(lambda row: len(row.split()))
+    bins = pd.cut(df["ReviewTextLength"], [0, 50, 100, 200, 300, float("inf")])
+    grouped_df = df.groupby(bins)
+    columns = [str(name).replace("300", ">300").replace(".0", "").replace(", ", "-").replace("-inf", "").replace("(",
+                                                                                                                 "").replace(
+        "[", "").replace("]", "").replace(")", "").replace(")", "").replace(" ", "").replace("->", "-") for name, group
+               in grouped_df]
+    output_df = pd.DataFrame(columns=columns, index=["Mr0", "Mr1_cap", "Mr1_uncap", "Mr2", "Mr3", "Mr4"])
+    for name, group in grouped_df:
+        new_df = pd.DataFrame()
+        new_df["Mr0_Compound"] = group["Mr0"].apply(lambda x: x["compound"])
+        new_df["Mr1_cap_Compound"] = group["Mr1"].apply(lambda x: x.get('capitalised')["compound"])
+        new_df["Mr1_uncap_Compound"] = group["Mr1"].apply(lambda x: x.get('uncapitalised')["compound"])
+        new_df["Mr2_Compound"] = group["Mr2"].apply(lambda x: x["compound"])
+        new_df["Mr3_Compound"] = group["Mr3"].apply(lambda x: x["compound"])
+        new_df["Mr4_Compound"] = group["Mr4"].apply(lambda x: x["compound"])
+        mean_df = new_df.agg(["mean"])
+        for col in mean_df.columns:
+            output_df.loc[col.replace("_Compound", ""), str(name).replace("300", ">300").replace(".0", "").replace(", ",
+                                                                                                                   "-").replace(
+                "-inf", "").replace("(", "").replace("[", "").replace("]", "").replace(")", "").replace(")",
+                                                                                                        "").replace(" ",
+                                                                                                                    "").replace(
+                "->", "-")] = mean_df[col].iloc[0]
+    output_df["Mr#"] = output_df.index
+    ax = output_df.plot(x="Mr#", y=columns, kind="bar")
     ax.set_xlabel("Metamorphic Relation")
     ax.set_ylabel("Average Compound Value")
-    ax.legend(title = "Word Length")
+    ax.legend(title="Word Length")
     plt.savefig(join_base_path("column_graph.png"))
     plt.draw()
-
-
-def prepare_word_length(df):
-    output_df = pd.DataFrame()
-    calculate_df = pd.DataFrame()
-    x = 0
-    # this monster operation sorts everything into a new dataframe and sorts each comp value into its category
-    for index, row in tqdm(df.iterrows(),total=len(df.index), unit="rows"):
-        calculate_df.at[x, 'Mr#'] = "Mr0"
-        calculate_df.at[x + 1, "Mr#"] = "Mr1cap"
-        calculate_df.at[x + 2, "Mr#"] = "Mr2"
-        calculate_df.at[x + 3, "Mr#"] = "Mr3"
-        calculate_df.at[x + 4, "Mr#"] = "Mr4"
-        calculate_df.at[x + 5, "Mr#"] = "Mr1uncap"
-        calculate_df.at[x, "comp"] = row["Mr0"].get("compound")
-        calculate_df.at[x + 1, "comp"] = row["Mr1"].get('capitalised').get('compound')
-        calculate_df.at[x + 2, "comp"] = row["Mr2"].get("compound")
-        calculate_df.at[x + 3, "comp"] = row["Mr3"].get("compound")
-        calculate_df.at[x + 4, "comp"] = row["Mr4"].get("compound")
-        calculate_df.at[x + 5, "comp"] = row["Mr1"].get('uncapitalised').get("compound")
-        if len(row['ReviewText'].split()) < 50:
-            calculate_df.at[x, "cat"] = "cat1"
-            calculate_df.at[x+1, "cat"] = "cat1"
-            calculate_df.at[x+2, "cat"] = "cat1"
-            calculate_df.at[x+3, "cat"] = "cat1"
-            calculate_df.at[x+4, "cat"] = "cat1"
-            calculate_df.at[x+5, "cat"] = "cat1"
-
-        elif len(row['ReviewText'].split()) < 100:
-            calculate_df.at[x, "cat"] = "cat2"
-            calculate_df.at[x + 1, "cat"] = "cat2"
-            calculate_df.at[x + 2, "cat"] = "cat2"
-            calculate_df.at[x + 3, "cat"] = "cat2"
-            calculate_df.at[x + 4, "cat"] = "cat2"
-            calculate_df.at[x + 5, "cat"] = "cat2"
-
-        elif len(row['ReviewText'].split()) < 200:
-            calculate_df.at[x, "cat"] = "cat3"
-            calculate_df.at[x + 1, "cat"] = "cat3"
-            calculate_df.at[x + 2, "cat"] = "cat3"
-            calculate_df.at[x + 3, "cat"] = "cat3"
-            calculate_df.at[x + 4, "cat"] = "cat3"
-            calculate_df.at[x + 5, "cat"] = "cat3"
-
-        elif len(row['ReviewText'].split()) < 300:
-            calculate_df.at[x, "cat"] = "cat4"
-            calculate_df.at[x + 1, "cat"] = "cat4"
-            calculate_df.at[x + 2, "cat"] = "cat4"
-            calculate_df.at[x + 3, "cat"] = "cat4"
-            calculate_df.at[x + 4, "cat"] = "cat4"
-            calculate_df.at[x + 5, "cat"] = "cat4"
-
-        elif len(row['ReviewText'].split()) >= 300:
-            calculate_df.at[x, "cat"] = "cat5"
-            calculate_df.at[x + 1, "cat"] = "cat5"
-            calculate_df.at[x + 2, "cat"] = "cat5"
-            calculate_df.at[x + 3, "cat"] = "cat5"
-            calculate_df.at[x + 4, "cat"] = "cat5"
-            calculate_df.at[x + 5, "cat"] = "cat5"
-        x = x + 6
-
-    output_df.at[0, "Mr#"] = "Mr0"
-    output_df.at[1, "Mr#"] = "Mr1uncap"
-    output_df.at[2, "Mr#"] = "Mr1cap"
-    output_df.at[3, "Mr#"] = "Mr2"
-    output_df.at[4, "Mr#"] = "Mr3"
-    output_df.at[5, "Mr#"] = "Mr4"
-
-    output_df.insert(1, "cat1", [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], True)
-    output_df.insert(2, "cat2", [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], True)
-    output_df.insert(3, "cat3", [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], True)
-    output_df.insert(4, "cat4", [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], True)
-    output_df.insert(5, "cat5", [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], True)
-
-    rowcount = [0, 0, 0, 0, 0]
-
-    for index, row in tqdm(calculate_df.iterrows(),total=len(calculate_df.index), unit="rows"):
-        y = row["Mr#"][-1]
-
-        if y == "p":
-            y = row["Mr#"][-5]
-
-            if y == "u":
-                y = "1"
-            else:
-                y = "2"
-        elif int(y) > 0:
-            y = int(y)+1
-
-        # counts the total comp value per catergory
-        output_df.at[int(y), row["cat"]] += row["comp"]
-        rowcount[int(row["cat"][-1])-1] += 1
-
-    # Gets the average rowcount is amount of times cat occurs in the calcuate df and its divided by 6 because that is
-    # how many times that occurs
-    output_df["cat1"] = output_df["cat1"].div(rowcount[0] / 6)
-    output_df["cat2"] = output_df["cat2"].div(rowcount[1] / 6)
-    output_df["cat3"] = output_df["cat3"].div(rowcount[2] / 6)
-    output_df["cat4"] = output_df["cat4"].div(rowcount[3] / 6)
-    output_df["cat5"] = output_df["cat5"].div(rowcount[4] / 6)
-
-
-
-    return output_df
 
 
 def summary_table(test_pass):
